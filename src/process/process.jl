a093ece31bc9cb20f3b0e7b92a36d9c060e8102f
pazzo83 <uvapazzo@gmail.com>
2015-09-29 20:25:14 -0400

    making changes for v0.4 compatibility and speed up of stochastic process by specifying datatypes

--- src/process/process.jl
+++ src/process/process.jl
@@ -7,30 +7,30 @@ using Calendar
 #Stochastic processes
 abstract StochasticProcess
 
-evolve(sp::StochasticProcess, t0::Real, x0, dt::Real, dw) = 
+evolve(sp::StochasticProcess, t0::Float64, x0::Float64, dt::Float64, dw::Float64) =
 					apply_change(sp, apply_change(sp, x0 , drift_discretize(sp, t0, x0, dt)) , diffusion_discretize(sp, t0,x0,dt) * dw)
-apply_change(ps::StochasticProcess, x, dx) = x + dx
+apply_change(ps::StochasticProcess, x::Float64, dx::Float64) = x + dx
 
 covariance(sp::StochasticProcess, t0::Real, x0, dt::Real) = covariance_discretize(sp, t0, x0, dt)
 
 #Euler discretization functions
-drift_discretize(sp::StochasticProcess, t0::Real, x0, dt::Real) = drift(sp, t0, x0) .* dt
+drift_discretize(sp::StochasticProcess, t0::Float64, x0::Float64, dt::Float64) = drift(sp, t0, x0) * dt
 
-diffusion_discretize(sp::StochasticProcess, t0::Real, x0, dt::Real) = diffusion(sp, t0, x0) .* sqrt(dt)
+diffusion_discretize(sp::StochasticProcess, t0::Float64, x0::Float64, dt::Float64) = diffusion(sp, t0, x0) * sqrt(dt)
 
-function covariance_discretize(sp::StochasticProcess, t0::Real, x0, dt::Real) 
-	s= diffusion(sp, t0, x0) 
+function covariance_discretize(sp::StochasticProcess, t0::Real, x0, dt::Real)
+	s= diffusion(sp, t0, x0)
 	( s * s' ) .* dt
 end
 
 type GeometricBrownianMotion <: StochasticProcess
-	start::Real
-    mu::Real
-    sigma::Real
+	start::Float64
+    mu::Float64
+    sigma::Float64
 end
 
-diffusion(p::GeometricBrownianMotion, t, x) = p.sigma * x
-drift(p::GeometricBrownianMotion, t, x) = p.mu * x
+diffusion(p::GeometricBrownianMotion, t::Float64, x::Float64) = p.sigma * x
+drift(p::GeometricBrownianMotion, t::Float64, x::Float64) = p.mu * x
 x0(p::GeometricBrownianMotion) = p.start
 
 
@@ -47,12 +47,12 @@ apply_change(sp::GenericBlackScholesProcess, x, dx) = x + exp(dx)
 
 diffusion(sp::GenericBlackScholesProcess, t, x) = vol(sp.vol, t, x)
 
-function drift(sp::GenericBlackScholesProcess, t, x) 
+function drift(sp::GenericBlackScholesProcess, t, x)
 	t1 = t+.0001
     r=forward_rate(yts, t, t1, Continuous, NoFrequency)
     d=forward_rate(dts, t, t1, Continuous, NoFrequency)
     sigma = diffusion(sp, t, x)
     return r - d - 0.5 * sigma * sigma
-end 
+end
 
 end #module
